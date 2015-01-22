5a2a73172c16e04bb53ab9ce835b1d15d205071c
Martin Drohmann <mdrohmann@gmail.com>
2015-01-21 23:20:13 -0800

    [lines2d] implementation
    This is actually a scatter plot

--- src/lines2d.js
+++ src/lines2d.js
@@ -6,17 +6,50 @@ function Lines2dView(view) {
     this.model = view.model;
     this.container = view.container;
     this.axis = new Axis2d(view);
-    this.svg = this.axis.draw_area;
-    this.adapter = this.axis.adapter;
 }
 
 Lines2dView.prototype = {};
 
 Lines2dView.prototype.update = function () {
     this.axis.update();
+    var svg = this.axis.draw_area;
+    var adapter = this.axis.adapter();
 
-/*    var scatter_classes = ['circle', this.adapter.name()];
-    var scatters = axis.selectAll('.circle') */
+    var lines = svg.selectAll('.circle_container')
+        .data(adapter.data(), function(d) { return d.name; });
+
+    /* one graph for each model */
+    // enter
+    var lines_enter_update = lines.enter()
+      .append('g')
+        .attr('class', function(d) { return 'circle_container ' + d.name; });
+
+    // update not necessary
+
+    // exit
+    lines.exit()
+        .remove();
+
+    /* plot the graphs */
+    var scatters = lines_enter_update.selectAll('.circle')
+        .data(function (d) { return d3.transpose([d.x.map(d.xscale), d.y.map(d.yscale)]); });
+
+    // enter
+    var scatters_update_enter = scatters.enter()
+      .append('circle')
+        .attr('class', 'circle');
+
+    // update and enter
+    scatters_update_enter
+        .transition()
+        .duration(100)
+        .attr('r', 3)
+        .attr('cx', function(d) { return d[0]; })
+        .attr('cy', function(d) { return d[1]; });
+
+    // exit
+    scatters.exit()
+        .remove();
 };
 
 module.exports = Lines2dView;
