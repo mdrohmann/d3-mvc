9b0f0ed8a5dc03cc98ccc3c8ea61278d8795ecb0
Martin Drohmann <mdrohmann@gmail.com>
2015-01-23 02:51:15 -0800

    made examples fancier

--- examples/linear_plot.js
+++ examples/linear_plot.js
@@ -2,5 +2,6 @@ d3.json('./linear_plot.json', function(model) {
      var view = d3mvc.make_view(model, '#linear_plot');
      view.addConfiguration({type: 'lines'})
          .addConfiguration({type: 'scatter'})
+         .addConfiguration({type: 'legend'})
          .display();
 });
